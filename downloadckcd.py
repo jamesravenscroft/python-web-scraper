e4842ae014db94c7c2e68ff8224ea69e0387542e
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-11-16 19:20:46 -0800

    Create downloadckcd.py

---
+++ downloadckcd.py
@@ -0,0 +1,12 @@
+import requests, os, bs4
+ url="http://xkcd.com"
+ os.makedirs('xkcd', exist_ok=True)
+while not url.endwith ('#')
+#TODO: download the page
+# TODO: download the image
+# TODO: Save the img to /.xkcd
+#TODO: Get the Prev button's url.
+
+print('DOwnloading page %s... % url)
+res= requests.get(url)
+res.raise_for_status()
