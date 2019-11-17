dc4b8f19520280bb475d6dffd04c8a3ebd56d3e1
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-11-16 19:12:20 -0800

    Update import.py

--- import.py
+++ import.py
@@ -8,3 +8,17 @@ spanElem.get('some_nonexistent_addr')== None
 True
 spanElem.attrs
 {'id': 'author'}
+
+
+pElems= exampleSoup.select('p')
+str(pElems[0])
+'<p>Download my <strong> Python</strong> book from <a href=:http//inventwithoython.com">my website</a></p>'
+pElems[0[.getText()
+         Download my Python book from my website.'
+         str(pElems[1])
+         <p class="slogan">L earn Python the easy way!</p>'
+         pElems[1].getText()
+         'Learn Python the easy way!'
+         str(pElems[2])
+         <p>By <span id =ahother> JR3</span></p>
+         pElems[2].getText()
