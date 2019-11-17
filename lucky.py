c29caa0e0c6d9223b79c7e422aaaab2102bc9a89
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-11-16 19:15:50 -0800

    Create lucky.py

---
+++ lucky.py
@@ -0,0 +1,7 @@
+import request, sys, web browser, bs4
+
+print ('Googling...;) # display text while downloading the Google page res= requests.get('http://google.com/search?q=' + ' .jon(sys.argv[1])))
+res.raise_for_status()
+
+#TODO: Retreive top search result links.
+# TODO: Open a browser tab for each result
